499da36bc0a06fdd8f9de3999f5c75f43ab871ce
Thomas <tholmes59@gmail.com>
2020-09-24 09:14:52 -0700

    add loading... to be displayed while data is being fetched

--- src/App.js
+++ src/App.js
@@ -3,28 +3,20 @@ import './App.css';
 import Ticker from './Ticker'
 
 function App() {
-
+  const [loading, setLoading] = useState(true)
   const [company, setCompany] = useState([])
 
     // useEffect(() => {
-    //   async function fetchData() {
-    //     const companyData = await fetch('https://api-v2.intrinio.com/companies/AAPL/fundamentals?api_key=OmVjYjY2ODE4YmFkMjg3MjExNjcwZDU2MzYyMmM2OTQy')
-    //     .then(res => res.json())
-    //     .then(data => setCompany(data))
-    //       // setCompany({data: companyData})
-        
-    //   }
-    //   fetchData()
-     
-    // }, []);
-
-    // console.log(company)
 
     useEffect(() => {
-     
-       fetch('https://api-v2.intrinio.com/companies/AAPL/fundamentals?api_key=OmVjYjY2ODE4YmFkMjg3MjExNjcwZDU2MzYyMmM2OTQy')
-        .then(res => res.json())
-        .then(data => setCompany(data))
+     setLoading(true)
+      //  fetch('https://api-v2.intrinio.com/companies/GOOG/fundamentals?api_key=OmVjYjY2ODE4YmFkMjg3MjExNjcwZDU2MzYyMmM2OTQy')
+       fetch('https://api-v2.intrinio.com/companies/news?api_key=OmVjYjY2ODE4YmFkMjg3MjExNjcwZDU2MzYyMmM2OTQy')
+       .then(res => res.json())
+        .then(data => {
+          setLoading(false)
+          setCompany(data)
+        })
           // setCompany({data: companyData})
         
 
@@ -32,14 +24,18 @@ function App() {
      
     }, []);
 
-    // console.log(company)
+    console.log(company.news)
 
  
-
+  if(loading) {
+    return <p>loading...</p>
+  }
 
   return (
     <div className="App">
-        <p>{company.company && company.company.ticker}</p>
+        {company.news && company.news.map(x => 
+            <p>{x.title}</p>
+        )}
         <Ticker company={company}/>
 
     </div>
