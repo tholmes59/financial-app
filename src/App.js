c185c8d2a15da5ce042fc8cc480e06136ec985c3
Thomas <tholmes59@gmail.com>
2020-10-26 15:00:52 -0700

    add search ticker fetch request from financial modeling prep API

--- src/App.js
+++ src/App.js
@@ -2,10 +2,13 @@ import React, { useState, useEffect } from 'react'
 import './App.css';
 import Ticker from './compoments/Ticker'
 import News from './compoments/News';
+import SearchTicker from './compoments/SearchTicker'
 
 function App() {
   const [loading, setLoading] = useState(true)
   const [company, setCompany] = useState([])
+  const [price, setPrice] = useState([])
+  const [symbol, setSymbol] = useState([])
 
     // useEffect(() => {
 
@@ -19,14 +22,52 @@ function App() {
           setCompany(data)
         })
           // setCompany({data: companyData})
-        
-
-     
-     
     }, []);
 
     // console.log(company.news)
 
+    useEffect(() => {
+      setLoading(true)
+        fetch('https://www.alphavantage.co/query?function=OVERVIEW&symbol=GOOGL&outputsize=full&apikey=6QNU5ORX2JVNZQDT')
+        .then(res => res.json())
+         .then(data => {
+           setLoading(false)
+           setPrice(data)
+         })
+           // setCompany({data: companyData})
+     }, []);
+
+    //  console.log(price)
+
+     async function fetchTicker(e) {
+      let val = e.target.symbol.value
+      console.log(val)
+      e.preventDefault() 
+      
+        setLoading(true)
+          const symbolData = await fetch(`https://financialmodelingprep.com/api/v3/search?query=${val}&apikey=3981e8e851120273545312697c324333`)
+          .then(res => res.json())
+           .then(data => data)
+             setLoading(false)
+             setSymbol({data: symbolData})
+           
+           
+     
+
+      }
+      //  useEffect(() => {
+      //   setLoading(true)
+      //     fetch(`https://financialmodelingprep.com/api/v3/search?query=${ticker}&apikey=3981e8e851120273545312697c324333`)
+      //     .then(res => res.json())
+      //      .then(data => {
+      //        setLoading(false)
+      //        setSymbol(data)
+      //      })
+      //        // setCompany({data: companyData})
+      //  }, []);
+
+
+     console.log(symbol.data)
  
   if(loading) {
     return <p>loading...</p>
@@ -34,8 +75,9 @@ function App() {
 
   return (
     <div className="App">
-        <Ticker company={company}/>
-        <News news={company} />
+        <SearchTicker getTicker={fetchTicker}/>
+        {/* <Ticker company={company}/>
+        <News news={company} /> */}
     </div>
   );
 }
