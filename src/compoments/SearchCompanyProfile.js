fc8fd1bb514841462ef3c872b981df1ec8cb67c2
Thomas <tholmes59@gmail.com>
2020-10-29 08:37:48 -0700

    create company search component

---
+++ src/compoments/SearchCompanyProfile.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const SearchCompanyProfile = (props) => {
+
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default SearchCompanyProfile;
