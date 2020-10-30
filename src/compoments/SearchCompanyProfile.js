0fcd3ab281afe7e49ff3152b1371cf67902f2fe8
Thomas <tholmes59@gmail.com>
2020-10-30 10:16:03 -0700

    addsearch form to serach company profile component

--- src/compoments/SearchCompanyProfile.js
+++ src/compoments/SearchCompanyProfile.js
@@ -4,7 +4,12 @@ const SearchCompanyProfile = (props) => {
 
     return (
         <div>
-            
+             <form onSubmit={props.getCompanyProfile}>
+                <input type="text" name="ticker" placeholder="Enter Ticker..."/>
+                <button value="Search" type="submit">
+                Search
+                </button>
+            </form>
         </div>
     )
 }
