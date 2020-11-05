226e0de3518d9b988de5a2ece6cff07af203fb85
Thomas <tholmes59@gmail.com>
2020-11-05 13:32:29 -0800

    fix functions to get news api call when ticker is placed

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import './App.css';
 import Ticker from './compoments/Ticker'
-import NewsData from './compoments/NewsData';
+import News from './compoments/News';
 import SearchTicker from './compoments/SearchTicker'
 import TickerResults from './compoments/TickerResults'
 import SearchCompanyProfile from './compoments/SearchCompanyProfile';
@@ -13,21 +13,6 @@ function App() {
   const [symbol, setSymbol] = useState([])
   const [news, setNews] = useState([])
 
-    // useEffect(() => {
-
-    // useEffect(() => {
-    //  setLoading(true)
-    //   //  fetch('https://api-v2.intrinio.com/companies/GOOG/fundamentals?api_key=OmVjYjY2ODE4YmFkMjg3MjExNjcwZDU2MzYyMmM2OTQy')
-    //    fetch('https://api-v2.intrinio.com/companies/news?api_key=OmVjYjY2ODE4YmFkMjg3MjExNjcwZDU2MzYyMmM2OTQy')
-    //    .then(res => res.json())
-    //     .then(data => {
-    //       setLoading(false)
-    //       setCompany(data)
-    //     })
-    //       setCompany({data: companyData})
-    // }, []);
-
-    // console.log(company.news)
 
     // useEffect(() => {
     //  setLoading(true)
@@ -58,7 +43,7 @@ function App() {
 
     async function fetchTicker(e) {
       let val = e.target.symbol.value
-      console.log(val)
+      // console.log(val)
       e.preventDefault() 
       
       // setLoading(true)
@@ -78,7 +63,7 @@ function App() {
       //      })
       //        // setCompany({data: companyData})
       //  }, []);
-     console.log(symbol.data) 
+    //  console.log(symbol.data) 
 
      async function fetchCompanyProfile(e) {
       let company = e.target.ticker.value
@@ -92,58 +77,22 @@ function App() {
             // setLoading(false)
             setCompany({data: companyData})
 
-        // let name = company.data && company.data.map(x => 
-        //   x.companyName.split(' ').shift()
-        //   )
-        // console.log(name)
-        // const companyNews = await fetch(`https://newsapi.org/v2/top-headlines?q=${name}&apiKey=236f90419a1d4edd8fc1e698c62220af`)
-        // .then(res => res.json())
-        // .then(data => data)
-        //   // setLoading(false)
-        //   setNews({data: companyNews})
+        let name = companyData && companyData.map(x => 
+          x.companyName.split(' ').shift()
+          )
+        console.log(name)
+        fetchCompanyNews(name)
     }
 
     console.log(company.data)
 
-    // console.log(company.data && company.data.map(x => 
-    //   x.companyName.split(' ')[0]
-    //   ))
-    
-    // let name = company.data && company.data.map(x => 
-    //     x.companyName.split(' ').shift()
-    //     )
-
-    // let API_URL = `https://newsapi.org/v2/top-headlines?q=${name}&apiKey=236f90419a1d4edd8fc1e698c62220af`
-
-    // useEffect(() => {
-    //   loadNews();
-    // }, []);
-
-    // async function fetchCompanyNews() {
-
-    //     const companyNews = await fetch(`https://newsapi.org/v2/top-headlines?q=${name}&apiKey=236f90419a1d4edd8fc1e698c62220af`)
-    //       .then(res => res.json())
-    //       .then(data => data)
-    //         setNews({data: companyNews})
-    // }
-    // console.log(name)
-    // useEffect(() => {
-    
-    //    fetch(`https://newsapi.org/v2/top-headlines?q=${name}&apiKey=236f90419a1d4edd8fc1e698c62220af`)
-    //    .then(res => res.json())
-    //     .then(data => {
-     
-    //       setNews(data)
-    //     })
-    //       setNews({data: news})
-    // }, []);
-    // console.log(news.data)
- 
-    // const loadNews = async () => {
-    //   const response = await fetch(API_URL);
-    //   const data = await response.json();
-    //   setNews(data)
-    // }
+    async function fetchCompanyNews(e) {
+        let name = e
+        const companyNews = await fetch(`https://newsapi.org/v2/top-headlines?q=${name}&apiKey=236f90419a1d4edd8fc1e698c62220af`)
+          .then(res => res.json())
+          .then(data => data)
+            setNews({data: companyNews})
+    }
 
     console.log(news)
   // if(loading) {
@@ -153,10 +102,11 @@ function App() {
   return (
     <div className="App">
         <SearchTicker getTicker={fetchTicker}/>
-        <SearchCompanyProfile getCompanyProfile={fetchCompanyProfile} />
+        <SearchCompanyProfile getCompanyProfile={fetchCompanyProfile}/>
         <TickerResults ticker={symbol}/>
+        <News news={news}/>
         {/* <Ticker company={company}/> */}
-        <NewsData news={company} />
+        {/* <NewsData news={company} /> */}
     </div>
   );
 }
