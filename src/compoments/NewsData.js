b907a337bb7efa7abafb57182f8e4dd1ddd75b07
Thomas <tholmes59@gmail.com>
2020-11-10 13:48:56 -0800

    remove function of news data component as it is no longer needed

--- src/compoments/NewsData.js
+++ src/compoments/NewsData.js
@@ -3,8 +3,8 @@ import News from './News'
 
 const NewsData = (props) => {
     // console.log(props.news.news && props.news.news)
-    const [news, setNews] = useState([])
-    console.log(props)
+    // const [news, setNews] = useState([])
+    // console.log(props)
 
     // let name = props.news.data && props.news.data.map(x => 
     //     x.companyName.split(' ').shift()
@@ -35,24 +35,35 @@ const NewsData = (props) => {
     //           setNews({data: news})
     //     }, []);
 
-        let name = props.news.data && props.news.data.map(x => 
-                x.companyName.split(' ').shift()
-                )
-        console.log(name)
-            let API_URL = `https://newsapi.org/v2/top-headlines?q=${name}&apiKey=236f90419a1d4edd8fc1e698c62220af`
-        
-            useEffect(() => {
-              loadNews();
-            }, []);
+        // let name = props.news.data && props.news.data.map(x => 
+        //         x.companyName.split(' ').shift()
+        //         )
+        // console.log(name)
+        //     let API_URL = `https://newsapi.org/v2/top-headlines?q=${name}&apiKey=236f90419a1d4edd8fc1e698c62220af`
+       
+        //     useEffect(() => {
+        //       loadNews();
+        //     }, []);
 
+            
+        // const loadNews = async () => {
+        //     console.log(API_URL)
+        //     const response = await fetch(API_URL);
+        //     console.log(response)
+        //     const data = await response.json();
+        //     setNews(data)
+        // }
+        // const loadNews = (API_URL) => {
+        //     console.log(API_URL)
+        //     fetch(API_URL)
+        //       .then(res => res.json())
+        //       .then(data => {
+        //           setNews(data)
+        //       })
 
-        const loadNews = async () => {
-            const response = await fetch(API_URL);
-            const data = await response.json();
-            setNews(data)
-        }
+        // }
 
-    console.log(news)
+    // console.log(news)
     // const companyNews = 
     //     <News news={fetchCompanyNews()}/>
         
