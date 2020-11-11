2ce84a710a7bcb0384179fe54c7d60e63ac4af86
Thomas <tholmes59@gmail.com>
2020-11-11 11:06:53 -0800

    get news card to display with correct API data

--- src/compoments/NewsCard.js
+++ src/compoments/NewsCard.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 
 const NewsCard = (props) => {
-
+    console.log(props)
     return (
         <div>
             <p><a href={props.news.url} target="_blank" rel="noopener noreferrer">{props.news.title}</a></p>
-            <p>{props.news.summary}</p>
+            <p>{props.news.description}</p>
         </div>
     )
 }
