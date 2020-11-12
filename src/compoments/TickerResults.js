1b5edca164432e8ca72e08dbc38bbd7a84b2c3a9
Thomas <tholmes59@gmail.com>
2020-11-12 13:12:01 -0800

    add company name to the ticker search

--- src/compoments/TickerResults.js
+++ src/compoments/TickerResults.js
@@ -4,7 +4,7 @@ const TickerResults = (props) => {
     console.log(props)
     return (
         <div>
-            {props.ticker.data && props.ticker.data.map(x => <li>{x.symbol}</li>)}
+            {props.ticker.data && props.ticker.data.map(x => <p>{x.symbol} | {x.name}</p>)}
         </div>
     )
 }
