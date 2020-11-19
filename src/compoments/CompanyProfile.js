00e3bffb6940e064b8935c5ab5de317738fb90bf
Thomas <tholmes59@gmail.com>
2020-11-19 08:20:44 -0800

    add logo to company profile

--- src/compoments/CompanyProfile.js
+++ src/compoments/CompanyProfile.js
@@ -2,11 +2,12 @@ import React from 'react'
 
 
 const CompanyProfile = props => {
-
+console.log(props)
 
     return (
         <div>
-            
+            Test Company Data
+           {props.company.data && <img src={`https://logo.clearbit.com/${props.company.data.map(x => x.website)}`}></img>}
         </div>
     )
 }
