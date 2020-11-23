a6421f45279edb6663c165a7e8ba32da9b0d627f
Thomas <tholmes59@gmail.com>
2020-11-23 10:29:56 -0800

    add inital stock chart layut to chart component

--- src/compoments/StockChart.js
+++ src/compoments/StockChart.js
@@ -3,9 +3,21 @@ import ReactHighcharts from 'react-highcharts/ReactHighstock.src'
 
 const StockChart = (props) => {
     console.log(props.price.data && props.price.data.historical.map(x => x.close))
+
+    const config = {
+        title: {
+          text: 'My stock chart'
+        },
+        series: [
+          {
+            data: props.price.data && (props.price.data.historical.map(x => x.close)).reverse()
+          }
+        ]
+      };
+
     return (
         <div>
-            
+            <ReactHighcharts config = {config}></ReactHighcharts>
         </div>
     )
 }
