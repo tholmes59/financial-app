68e826d5e0e0913e891f874fcc79dfec1614738b
Thomas <tholmes59@gmail.com>
2020-11-24 09:14:54 -0800

    create array and manipulate API price data to work with highcharts

--- src/compoments/StockChart.js
+++ src/compoments/StockChart.js
@@ -2,7 +2,23 @@ import React from 'react'
 import ReactHighcharts from 'react-highcharts/ReactHighstock.src'
 
 const StockChart = (props) => {
+    console.log(props.price.data && props.price.data)
     console.log(props.price.data && props.price.data.historical.map(x => x.close))
+
+    let dataArray = []
+
+    let stockData = props.price.data && props.price.data.historical.reverse()
+    
+    if(stockData){
+        for(let i=0; i<stockData.length; i++){
+            let tempArray = []
+            tempArray.push(new Date(stockData[i].date).getTime(), stockData[i].close)
+            dataArray.push(tempArray)
+            tempArray = []
+        }
+    }
+
+    console.log(dataArray)
 
     const config = {
         title: {
@@ -10,9 +26,33 @@ const StockChart = (props) => {
         },
         series: [
           {
-            data: props.price.data && (props.price.data.historical.map(x => x.close)).reverse()
+            data: dataArray
           }
-        ]
+        ],
+        rangeSelector: {
+            buttons: [{
+              type: 'day',
+              count: 1,
+              text: '1d',
+            }, {
+              type: 'day',
+              count: 7,
+              text: '7d'
+            }, {
+              type: 'month',
+              count: 1,
+              text: '1m'
+            }, {
+              type: 'month',
+              count: 3,
+              text: '3m'
+            },
+              {
+              type: 'all',
+              text: 'All'
+            }],
+            selected: 4
+          },
       };
 
     return (
