f769df17950d6e9536fc2f5b3fc410ef4203d624
Thomas <tholmes59@gmail.com>
2020-12-09 14:33:46 -0800

    add additional company info to company profile

--- src/compoments/CompanyProfile.js
+++ src/compoments/CompanyProfile.js
@@ -7,6 +7,10 @@ console.log(props)
     return (
         <div>
             {props.company.data && <img alt="CompanyLogo" src={props.company.data.map(x => x.image)}></img>}
+            {props.company.data && props.company.data.map(x => <p>{x.companyName}</p>)}
+            {props.company.data && props.company.data.map(x => <p>{x.address}</p>)}
+            {props.company.data && props.company.data.map(x => <p>{x.city}, {x.state} {x.zip}</p>)}
+            {props.company.data && props.company.data.map(x => <p>{x.ceo}</p>)}
         </div>
     )
 }
