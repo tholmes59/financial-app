0e5976d0c70b15ed546a79dea4090ff03ee6acb6
Thomas <tholmes59@gmail.com>
2020-12-11 09:22:52 -0800

    test passing props to company metrics component and start displaying data

--- src/compoments/CompanyMetrics.js
+++ src/compoments/CompanyMetrics.js
@@ -1,10 +1,11 @@
 import React from 'react'
 
 const CompanyMetrics = (props) => {
-
+    console.log(props)
+    console.log(props.metrics)
     return(
         <div>
-            
+            {props.metrics.data && props.metrics.data.map(x => <p>Market Cap: {x.marketCapTTM}</p>)}
         </div>
     )
 }
