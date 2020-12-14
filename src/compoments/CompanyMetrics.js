4638066cc00d086df783841c8efc922805b7a8b5
Thomas <tholmes59@gmail.com>
2020-12-14 13:45:25 -0800

    add logic to abreviate market cap based on the saze of the company

--- src/compoments/CompanyMetrics.js
+++ src/compoments/CompanyMetrics.js
@@ -3,6 +3,17 @@ import React from 'react'
 const CompanyMetrics = (props) => {
     console.log(props)
     console.log(props.metrics)
+    let marketCap;
+    if((props.metrics.data && props.metrics.data.map(x => x.marketCapTTM)) < 1000000000){
+        marketCap = ((props.metrics.data && props.metrics.data.map(x => x.marketCapTTM))/1000000) + 'M'
+    }
+    if((props.metrics.data && props.metrics.data.map(x => x.marketCapTTM)) < 1000000000000){
+        marketCap = ((props.metrics.data && props.metrics.data.map(x => x.marketCapTTM))/1000000000) + 'B'
+    }
+    else {
+        marketCap = ((props.metrics.data && props.metrics.data.map(x => x.marketCapTTM))/1000000000000) + 'T'
+    }
+    console.log(marketCap)
     return(
         <div>
             {props.metrics.data && props.metrics.data.map(x => <p>Market Cap: {x.marketCapTTM}</p>)}
