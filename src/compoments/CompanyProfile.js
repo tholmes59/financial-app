cd98430c6b3240c3ddabb6730db054c22379cf1e
Thomas <tholmes59@gmail.com>
2020-12-15 10:19:01 -0800

    add proce change and percent change to daily values and change layout of Market Cap display

--- src/compoments/CompanyProfile.js
+++ src/compoments/CompanyProfile.js
@@ -5,22 +5,52 @@ const CompanyProfile = props => {
 console.log(props)
 
 let marketCap;
-    if((props.company.data && props.company.data.map(x => x.mktCap)) < 1000000000){
-        marketCap = ((props.company.data && props.company.data.map(x => x.mktCap))/1000000).toFixed(2) + 'M'
-    }
-    if((props.company.data && props.company.data.map(x => x.mktCap)) < 1000000000000){
-        marketCap = ((props.company.data && props.company.data.map(x => x.mktCap))/1000000000).toFixed(2) + 'B'
-    }
-    else {
-        marketCap = ((props.company.data && props.company.data.map(x => x.mktCap))/1000000000000).toFixed(3) + 'T'
-    }
-    console.log(marketCap)
-    
+if (
+  (props.company.data && props.company.data.map((x) => x.mktCap)) < 1000000000
+) {
+  marketCap =
+    (
+      (props.company.data && props.company.data.map((x) => x.mktCap)) / 1000000
+    ).toFixed(2) + "M";
+}
+if (
+  (props.company.data && props.company.data.map((x) => x.mktCap)) <
+  1000000000000
+) {
+  marketCap =
+    (
+      (props.company.data && props.company.data.map((x) => x.mktCap)) /
+      1000000000
+    ).toFixed(2) + "B";
+} else {
+  marketCap =
+    (
+      (props.company.data && props.company.data.map((x) => x.mktCap)) /
+      1000000000000
+    ).toFixed(3) + "T";
+}
+console.log(marketCap);
+
+let price = props.company.data && (props.company.data.map(x =>x.price).shift()).toFixed(2)*1
+let priceChange = props.company.data && (props.company.data.map(x =>x.changes).shift()).toFixed(2)*1
+let percentChange = ((((price + priceChange) / price) -1 )*100).toFixed(2) + '%'
+// let percentChange = (((price + priceChange) / price) - 1) 
+console.log(price)
+console.log(priceChange)
+console.log(price + priceChange)
+let pricePercentageFontColor = ''
+if(priceChange > 0){
+    pricePercentageFontColor = {color: "green"}
+} 
+if(priceChange < 0){
+    pricePercentageFontColor = {color: "red"}
+}
+console.log(percentChange)   
     return (
         <div>
             {props.company.data && <img alt="CompanyLogo" src={props.company.data.map(x => x.image)}></img>}
             {props.company.data && props.company.data.map(x => <p>{x.companyName} ({x.symbol})</p>)}
-            {props.company.data && props.company.data.map(x => <p>{x.price} {x.changes}</p>)}
+            {props.company.data && <p>{price} {<span style={pricePercentageFontColor}>{priceChange}</span>} {<span style={pricePercentageFontColor}>({percentChange})</span>}</p>}
             {props.company.data && props.company.data.map(x => <p>{x.address}</p>)}
             {props.company.data && props.company.data.map(x => <p>{x.city}, {x.state} {x.zip}</p>)}
             {props.company.data && props.company.data.map(x => <p>{x.ceo}</p>)}
