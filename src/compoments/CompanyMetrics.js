23058d6e34aa061e6f7a5995a9444188973096ff
Thomas <tholmes59@gmail.com>
2020-12-18 14:09:11 -0800

    create finction to extrapolate the handling of a null value

--- src/compoments/CompanyMetrics.js
+++ src/compoments/CompanyMetrics.js
@@ -4,16 +4,26 @@ const CompanyMetrics = (props) => {
     console.log(props)
     console.log(props.metrics)
     
-    let peRatio = props.metrics.data && props.metrics.data.map(x => x.peRatioTTM).shift()
-    if(!peRatio) {
-        peRatio = 'N/A'
-    } else {
-        peRatio = peRatio.toFixed(2);
+    // let peRatio = props.metrics.data && props.metrics.data.map(x => x.peRatioTTM).shift()
+    // if(!peRatio) {
+    //     peRatio = 'N/A'
+    // } else {
+    //     peRatio = peRatio.toFixed(2);
+    // }
+    // console.log(peRatio)
+    function companyMetrics(metric){
+        let value = metric.shift()
+        if(!value) {
+            return 'N/A'
+        } else {
+            return value.toFixed(2);
+        }
     }
-    console.log(peRatio)
     return(
         <div>
-            {props.metrics.data && <p>P/E Ratio: {peRatio}</p>}
+            {/* {props.metrics.data && <p>P/E Ratio: {peRatio}</p>} */}
+            {props.metrics.data && <p>P/E Ratio: {companyMetrics(props.metrics.data && props.metrics.data.map(x => (x.peRatioTTM)))}</p>}
+            {props.metrics.data && <p>PEG Ratio: {companyMetrics(props.metrics.data && props.metrics.data.map(x => (x.pegRatioTTM)))}</p>}
             {/* {props.metrics.data && props.metrics.data.map(x => <p>Price to Earnings: {(x.peRatioTTM).toFixed(2)}</p>)}
             {props.metrics.data && props.metrics.data.map(x => <p>PEG Ratio: {(x.pegRatioTTM).toFixed(2)}</p>)}
             {props.metrics.data && props.metrics.data.map(x => <p>Price to Book: {(x.priceToBookRatioTTM).toFixed(2)}</p>)}
