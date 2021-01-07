8c13be2f8c0ddc5a1d68e618047fe97fd76fd3af
Thomas <tholmes59@gmail.com>
2021-01-07 11:47:54 -0800

    edit styling and date using moment.js

--- src/compoments/NewsCard.js
+++ src/compoments/NewsCard.js
@@ -1,16 +1,17 @@
 import React from 'react';
+import moment from 'moment'
 
 
 const NewsCard = (props) => {
     console.log(props)
     return (
-        <div className="news-container">
+        <div className="news-card-container">
             <p><a href={props.news.url} target="_blank" rel="noopener noreferrer" className="news-title">{props.news.title}</a></p>
             <div>
                 By {props.news.author}
             </div>
             <div>
-                {props.news.source.name} - {props.news.publishedAt}
+                {props.news.source.name} - {moment(props.news.publishedAt).format('LLL')}
             </div>
             <div>{props.news.description}</div>
         </div>
