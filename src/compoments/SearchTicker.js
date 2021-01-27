f90906ba2824cdcb58090ac1edacb81c73461004
Thomas <tholmes59@gmail.com>
2021-01-27 08:36:21 -0800

    fix typo in placeholder

--- src/compoments/SearchTicker.js
+++ src/compoments/SearchTicker.js
@@ -6,7 +6,7 @@ const SearchTicker = (props) => {
     return (
         <div>
             <form onSubmit={props.getTicker} className="search-form">
-                <input type="text" name="symbol" placeholder="Enter Compamy Name..." />
+                <input type="text" name="symbol" placeholder="Enter Company Name..." />
                 <button value="Search Ticker" type="submit">
                     <i class="fa fa-search"></i>
                 </button>
