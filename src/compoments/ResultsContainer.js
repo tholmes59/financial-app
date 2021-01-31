0fc3b917513ccb396faf7ee5133c7d62249c518f
Thomas <tholmes59@gmail.com>
2021-01-31 14:38:37 -0800

    add loadspinner componeent to results container

--- src/compoments/ResultsContainer.js
+++ src/compoments/ResultsContainer.js
@@ -3,11 +3,16 @@ import CompanyProfile from './CompanyProfile'
 import StockChart from './StockChart'
 import CompanyMetrics from './CompanyMetrics'
 import News from './News'
+import LoadingSpinner from './LoadingSpinner'
 
 const ResultsContainer = ({company, metrics, news, price, loading}) => {
    
     if (loading) {
-        return <h2>Loading...</h2>
+        return (
+            <div className="spinner-container">
+                <LoadingSpinner/>
+            </div>
+            )
     }
     return(
         <div className="results-container">
