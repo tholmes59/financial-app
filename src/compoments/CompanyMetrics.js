d5cce6b44ea0f34b72a0e6578ff9f473cbbde834
Thomas <tholmes59@gmail.com>
2021-02-05 12:10:23 -0800

    fix error and add header got title and table headers

--- src/compoments/CompanyMetrics.js
+++ src/compoments/CompanyMetrics.js
@@ -18,11 +18,11 @@ const CompanyMetrics = (props) => {
         <>
         {props.metrics.data && (
             <div className="valuation-metrics-container">
-                <div>Valuation Metrics</div>
-                    <div>
+                <h1>Valuation Metrics</h1>
+                    <h3>
                         Profitability Ratios
-                    </div>
-                <table>
+                    </h3>
+                    <table>
                         {props.metrics.data && <tr><td>Gross Profit Margin:</td> <td>{companyMetrics('grossProfitMarginTTM')}</td></tr>}
                         {props.metrics.data && <tr><td>Operating Profit Margin:</td> <td>{companyMetrics('operatingProfitMarginTTM')}</td></tr>}
                         {props.metrics.data && <tr><td>Pretax Profit Margin:</td> <td>{companyMetrics('pretaxProfitMarginTTM')}</td></tr>}
@@ -33,19 +33,19 @@ const CompanyMetrics = (props) => {
                         {props.metrics.data && <tr><td>EBIT per Revenue:</td> <td>{companyMetrics('ebitPerRevenueTTM')}</td></tr>}
                         {props.metrics.data && <tr><td>EBT per EBIT:</td> <td>{companyMetrics('ebtPerEbitTTM')}</td></tr>}
                         {props.metrics.data && <tr><td>Net Income per EBT:</td> <td>{companyMetrics('netIncomePerEBTTTM')}</td></tr>}
-                        </table>
-                    <div>
+                    </table>
+                    <h3>
                         Liquidity Ratios
-                    </div>
+                    </h3>
                     <table>
                         {props.metrics.data && <tr><td>Cash Ratio:</td> <td>{companyMetrics('cashRatioTTM')}</td></tr>}
                         {props.metrics.data && <tr><td>Quick Ratio:</td> <td>{companyMetrics('quickRatioTTM')}</td></tr>}
                         {props.metrics.data && <tr><td>Current Ratio:</td> <td>{companyMetrics('currentRatioTTM')}</td></tr>}
                         {props.metrics.data && <tr><td>Short Term Coverage Ratio:</td> <td>{companyMetrics('shortTermCoverageRatiosTTM')}</td></tr>}
                     </table>
-                    <div>
+                    <h3>
                         Activity Ratios
-                    </div>
+                    </h3>
                         {props.metrics.data && <div>Receivables Turnover: {companyMetrics('receivablesTurnoverTTM')}</div>}
                         {props.metrics.data && <div>Days Sales Outstanding: {companyMetrics('daysOfSalesOutstandingTTM')}</div>}
                         {props.metrics.data && <div>Inventory Turnover: {companyMetrics('inventoryTurnoverTTM')}</div>}
@@ -57,9 +57,9 @@ const CompanyMetrics = (props) => {
                         {props.metrics.data && <div>Days Payables Outstanding: {companyMetrics('daysOfPayablesOutstandingTTM')}</div>}
                         {props.metrics.data && <div>Cash Conversion Cycle: {companyMetrics('cashConversionCycleTTM')}</div>}
                         {props.metrics.data && <div>Effective Tax Rate: {companyMetrics('effectiveTaxRateTTM')}</div>}
-                    <div>
+                    <h3>
                         Leverage Ratios
-                    </div>
+                    </h3>
                         {props.metrics.data && <div>Debt Ratio: {companyMetrics('debtRatioTTM')}</div>}
                         {props.metrics.data && <div>Debt to Equity Ratio: {companyMetrics('debtEquityRatioTTM')}</div>}
                         {props.metrics.data && <div>Interest Coverage Ratio: {companyMetrics('interestCoverageTTM')}</div>}
@@ -69,10 +69,10 @@ const CompanyMetrics = (props) => {
                         {props.metrics.data && <div>Long Term Debt to Capitalization: {companyMetrics('longTermDebtToCapitalizationTTM')}</div>}
                         {props.metrics.data && <div>Total Debt to Capitalization Ratio: {companyMetrics('totalDebtToCapitalizationTTM')}</div>}
                         {props.metrics.data && <div>Cash Flow to Debt Ratio: {companyMetrics('cashFlowToDebtRatioTTM')}</div>}
-                    {props.metrics.data && <div>Company Equity Multiplier: {companyMetrics('companyEquityMultiplierTTM')}</div>}
-                    <div>
+                        {props.metrics.data && <div>Company Equity Multiplier: {companyMetrics('companyEquityMultiplierTTM')}</div>}
+                    <h3>
                         Market Ratios
-                    </div>
+                    </h3>
                         {props.metrics.data && <div>P/E Ratio: {companyMetrics('peRatioTTM')}</div>}
                         {props.metrics.data && <div>PEG Ratio: {companyMetrics('pegRatioTTM')}</div>}
                         {props.metrics.data && <div>P/B: {companyMetrics('priceToBookRatioTTM')}</div>}
@@ -90,7 +90,6 @@ const CompanyMetrics = (props) => {
                         {props.metrics.data && <div>Free Cash Flow to Operating Cash Flow Ratio: {companyMetrics('freeCashFlowOperatingCashFlowRatioTTM')}</div>}
                         {props.metrics.data && <div>Operating Cash Flow to Sales Ratio: {companyMetrics('operatingCashFlowSalesRatioTTM')}</div>}
                         {props.metrics.data && <div>Enterprise Value Multiple: {companyMetrics('enterpriseValueMultipleTTM')}</div>}
-                
             </div>
         )}
         </>
