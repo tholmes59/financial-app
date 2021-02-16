0a1da55ad81f61825c970454397400bba72c8dc4
Thomas <tholmes59@gmail.com>
2021-02-16 11:38:22 -0800

    add quote api to header to fetch randow quote

--- src/compoments/Header.js
+++ src/compoments/Header.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 
 const Header = () => {
+
+    const [quote, setQuote] = useState([])
+
+    useEffect(() => {
+        const loadQuote = async () => {
+            const quote = await fetch(`https://api.quotable.io/random`)
+            .then(res => res.json())
+            .then(data => data)
+            setQuote({data: quote})
+        }
+        loadQuote()
+    }, [])
+
+    console.log(quote)
 
     return(
         <header>
