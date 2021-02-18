7daa57b88af36121e319f25faad0dead84a26e21
Thomas <tholmes59@gmail.com>
2021-02-18 10:35:50 -0800

    add image to news stories and add classnames to div's for styling

--- src/compoments/NewsCard.js
+++ src/compoments/NewsCard.js
@@ -5,16 +5,22 @@ import moment from 'moment'
 const NewsCard = (props) => {
     console.log(props)
     return (
-        <div className="news-card-container">
-            <p><a href={props.news.url} target="_blank" rel="noopener noreferrer" className="news-title">{props.news.title}</a></p>
-            <div>
-                By {props.news.author}
+            <div className="news-card-container">
+            <div className="news-image-container">
+                <img alt="CompanyLogo" src={props.news.urlToImage}></img>
             </div>
-            <div>
-                {props.news.source.name} - {moment(props.news.publishedAt).format('LLL')}
+            <div className="news-container">
+                <p><a href={props.news.url} target="_blank" rel="noopener noreferrer" className="news-title">{props.news.title}</a></p>
+                <div>
+                    By {props.news.author}
+                </div>
+                <div>
+                    {props.news.source.name} - {moment(props.news.publishedAt).format('LLL')}
+                </div>
+                <div>{props.news.description}</div>
+                
             </div>
-            <div>{props.news.description}</div>
-        </div>
+            </div>
     )
 }
 
