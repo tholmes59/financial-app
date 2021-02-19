caaecb3c2e5f8d34740becada9728941208dd0b4
Thomas <tholmes59@gmail.com>
2021-02-19 10:00:08 -0800

    add classnames to news card to improve styling

--- src/compoments/NewsCard.js
+++ src/compoments/NewsCard.js
@@ -5,23 +5,30 @@ import moment from 'moment'
 const NewsCard = (props) => {
     console.log(props)
     return (
-            <div className="news-card-container">
-            <div className="news-image-container">
-                <img alt="CompanyLogo" src={props.news.urlToImage}></img>
-            </div>
-            <div className="news-container">
-                <p><a href={props.news.url} target="_blank" rel="noopener noreferrer" className="news-title">{props.news.title}</a></p>
-                <div>
-                    By {props.news.author}
-                </div>
-                <div>
-                    {props.news.source.name} - {moment(props.news.publishedAt).format('LLL')}
-                </div>
-                <div>{props.news.description}</div>
-                
-            </div>
-            </div>
-    )
+      <div className="news-card-container">
+        <div className="news-image-container">
+          <img alt="CompanyLogo" src={props.news.urlToImage}></img>
+        </div>
+        <div className="news-container">
+          <p>
+            <a
+              href={props.news.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              className="news-title"
+            >
+              {props.news.title}
+            </a>
+          </p>
+          <div className="news-author">By {props.news.author}</div>
+          <div className="news-source">
+            {props.news.source.name} -{" "}
+            {moment(props.news.publishedAt).format("LLL")}
+          </div>
+          <div className="news-description">{props.news.description}</div>
+        </div>
+      </div>
+    );
 }
 
 export default NewsCard;
