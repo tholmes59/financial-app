25478131aec083990f9446434d235617216f868a
Thomas <tholmes59@gmail.com>
2021-02-23 11:19:07 -0800

    pass props and add pagenumbers and onclick event to make pagination function

--- src/compoments/Pagination.js
+++ src/compoments/Pagination.js
@@ -1,10 +1,29 @@
 import React from 'react';
 
-const Pagination = () => {
+const Pagination = ({ storiesPerPage, totalStories, paginate }) => {
+
+    console.log(storiesPerPage)
+    console.log(totalStories)
+
+    // let allStoriesNumber = totalStories && totalStories.length
+
+    let pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(totalStories/ storiesPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
     return (
-        <div>
-            
-        </div>
+        <nav>
+            <ul>
+                {pageNumbers.map(number => (
+                <li key={number}>
+                    <button onClick={() => paginate(number)} >
+                    {number}
+                    </button>
+                </li>
+                ))}
+            </ul>
+        </nav>
     );
 };
 
