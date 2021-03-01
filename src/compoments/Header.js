b903ba264ba5aacad155bc0b3ec54ec8d16907ce
Thomas <tholmes59@gmail.com>
2021-03-01 13:41:25 -0800

    remove quote api fetch and import quote component

--- src/compoments/Header.js
+++ src/compoments/Header.js
@@ -1,26 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import Quote from './Quote';
 
 
 const Header = () => {
 
-    const [quote, setQuote] = useState([])
+    // const [quote, setQuote] = useState([])
 
-    useEffect(() => {
-        const loadQuote = async () => {
-            const quote = await fetch(`https://api.quotable.io/random`)
-            .then(res => res.json())
-            .then(data => data)
-            setQuote(quote)
-        }
-        loadQuote()
-    }, [])
+    // useEffect(() => {
+    //     const loadQuote = async () => {
+    //         const quote = await fetch(`https://api.quotable.io/random`)
+    //         .then(res => res.json())
+    //         .then(data => data)
+    //         setQuote(quote)
+    //     }
+    //     loadQuote()
+    // }, [])
   
     return(
         <header>
             <div className="header-container">
                 <div className="header-title">InvestorInfo</div>
-                <div>"{quote.content}"</div>
-                <div>-{quote.author}</div>
+                {/* <div>"{quote.content}"</div>
+                <div>-{quote.author}</div> */}
+                <Quote/>
             </div>
         </header>
     )
