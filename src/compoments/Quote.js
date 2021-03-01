0246777e28dc872f80431b4c2b85bb04e544fe17
Thomas <tholmes59@gmail.com>
2021-03-01 12:11:58 -0800

    add laoding hook to quotes api fetch

--- src/compoments/Quote.js
+++ src/compoments/Quote.js
@@ -3,20 +3,24 @@ import React, {useState, useEffect} from 'react';
 function Quote() {
 
     const [quote, setQuote] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
         const loadQuote = async () => {
             const quote = await fetch(`https://api.quotable.io/random`)
             .then(res => res.json())
             .then(data => data)
             setQuote(quote)
+            setLoading(false)
         }
         loadQuote()
     }, [])
 
     return (
         <div>
-            
+            <div>"{quote.content}"</div>
+            <div>-{quote.author}</div>
         </div>
     );
 }
