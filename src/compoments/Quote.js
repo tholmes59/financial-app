0654624f8aea9eb9057bb294629b4d76f2036d17
Thomas <tholmes59@gmail.com>
2021-03-01 12:09:14 -0800

    add fetch of quote from quate api

--- src/compoments/Quote.js
+++ src/compoments/Quote.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Quote() {
 
+    const [quote, setQuote] = useState([])
+
+    useEffect(() => {
+        const loadQuote = async () => {
+            const quote = await fetch(`https://api.quotable.io/random`)
+            .then(res => res.json())
+            .then(data => data)
+            setQuote(quote)
+        }
+        loadQuote()
+    }, [])
 
     return (
         <div>
