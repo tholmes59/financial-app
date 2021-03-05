e606260d57c2149a34ed437ae3675b42ae7dca7d
Thomas <tholmes59@gmail.com>
2021-03-05 10:14:52 -0800

    add function to convert 52 week range to 2 decimals

--- src/compoments/CompanyProfile.js
+++ src/compoments/CompanyProfile.js
@@ -46,6 +46,16 @@ if(priceChange < 0){
     pricePercentageFontColor = {color: "red"}
 }
 console.log(percentChange)   
+
+const fiftyTwoWeek = (val) => {
+  console.log(val)
+  let temp = val[0].split('-')
+  console.log(temp)
+  let low = Number(temp[0]).toFixed(2)
+  let high = Number(temp[1]).toFixed(2)
+  return `${low} - ${high}`
+}
+
     return (
         <div>
           <div className="full-name-container">
@@ -62,7 +72,7 @@ console.log(percentChange)
               {props.company.data && props.company.data.map(x => <div>{x.ceo}</div>)}
             </div>
             {props.company.data && <div>Market Cap: {marketCap}</div>}
-            {props.company.data && <div>52 Week Range: {props.company.data.map(x => x.range)}</div>}
+            {props.company.data && <div>52 Week Range: {fiftyTwoWeek(props.company.data && props.company.data.map(x => x.range))}</div>}
             <div className="company-description">
               {props.company.data && props.company.data.map(x => <div>{x.description}</div>)}
             </div>
