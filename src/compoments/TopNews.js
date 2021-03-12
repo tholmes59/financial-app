d6468c2ec3982fbb9b4409b19368b03e4e24dfd1
Thomas <tholmes59@gmail.com>
2021-03-12 11:54:18 -0800

    add useeffect hook to fet top news from currents api

--- src/compoments/TopNews.js
+++ src/compoments/TopNews.js
@@ -1,6 +1,21 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-const TopNews = (props) => {
+const TopNews = () => {
+
+    const [topNews, setTopNews] = useState([])
+
+    useEffect(() => {
+        const loadTopNews = async () => {
+            const news = await fetch(`https://api.currentsapi.services/v1/latest-news?language=en&apiKey=jByhIdldcnJhFEpjNJXLMwQC3F2hU8FLz274joQg2fsxGHu5`)
+            .then(res => res.json())
+            .then(data => data)
+            setTopNews(news)
+        }
+        loadTopNews()
+    }, [])
+
+    console.log(topNews)
+   
     return (
         <div>
             
