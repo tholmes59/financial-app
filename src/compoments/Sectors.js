1112bce1315850509cab09f29e51f4be3ee3fbff
Thomas <tholmes59@gmail.com>
2021-03-16 09:23:34 -0700

    add fetch of sector data to sectors component

--- src/compoments/Sectors.js
+++ src/compoments/Sectors.js
@@ -1,6 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
-function Sectors(props) {
+function Sectors() {
+
+    const [sectors, setSectors] = useState([])
+
+    useEffect(() => {
+        const loadSectors = async () => {
+            const news = await fetch(`https://www.alphavantage.co/query?function=SECTOR&apikey=6QNU5ORX2JVNZQDT`)
+            .then(res => res.json())
+            .then(data => data)
+            setSectors(news)
+        }
+        loadSectors()
+    }, [])
+
+    console.log(sectors)
     return (
         <div>
             
